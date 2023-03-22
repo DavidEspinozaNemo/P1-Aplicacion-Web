0fdc6f5f682f7c41ff7d59532c7b5a1d7c76270e
DavidEspinozaNemo <david1998es16@gmail.com>
2023-03-22 16:56:39 -0600

    solución a error en modificar

--- SQL Files/p_CRUD.sql
+++ SQL Files/p_CRUD.sql
@@ -6,10 +6,10 @@ CREATE OR REPLACE PACKAGE package_CRUD AS
     PROCEDURE agregar_producto (v_nombre varchar2,v_descripcion varchar2, v_precio number);
     PROCEDURE agregar_hcompra (v_fecha date,v_costo number, v_cantidad number, v_provedorID number);
     PROCEDURE agregar_productoV (v_productoID number,v_ventaID number, v_precio number, v_cantidad number);
-    PROCEDURE modificar_proveedor(v_id NUMBER, v_nombre VARCHAR2, v_fecha DATE);
-    PROCEDURE modificar_producto(v_id NUMBER, v_nombre VARCHAR2, v_precio NUMBER, v_descp VARCHAR2);
-    PROCEDURE modificar_cliente(v_id NUMBER, v_nombre VARCHAR2, v_apellido1 VARCHAR2,  v_apellido2 VARCHAR2);
-    PROCEDURE modificar_empleado(v_id NUMBER, v_nombre VARCHAR2, v_apellido1 VARCHAR2, v_apellido2 VARCHAR2, v_salario NUMBER, v_correo VARCHAR2, v_contacto VARCHAR2);
+    PROCEDURE modificar_proveedor(v_id NUMBER, v_nombre VARCHAR2 DEFAULT 'NULL', v_fecha DATE DEFAULT SYSDATE);
+    PROCEDURE modificar_producto(v_id NUMBER, v_nombre VARCHAR2 DEFAULT 'NULL', v_precio NUMBER DEFAULT 0, v_descp VARCHAR2 DEFAULT 'NULL');
+    PROCEDURE modificar_cliente(v_id NUMBER, v_nombre VARCHAR2 DEFAULT 'NULL', v_apellido1 VARCHAR2 DEFAULT 'NULL',  v_apellido2 VARCHAR2 DEFAULT 'NULL');
+    PROCEDURE modificar_empleado(v_id NUMBER, v_nombre VARCHAR2 DEFAULT 'NULL', v_apellido1 VARCHAR2 DEFAULT 'NULL', v_apellido2 VARCHAR2 DEFAULT 'NULL', v_salario NUMBER DEFAULT 0, v_correo VARCHAR2 DEFAULT 'NULL', v_contacto VARCHAR2 DEFAULT 'NULL');
 END package_CRUD;
 /
 
@@ -95,73 +95,106 @@ AS
 	
 	-- procedimiento para modificar
 	
-	PROCEDURE modificar_proveedor(v_id NUMBER, v_nombre VARCHAR2, v_fecha DATE) AS
+	PROCEDURE modificar_proveedor(v_id NUMBER, v_nombre VARCHAR2 DEFAULT 'NULL', v_fecha DATE DEFAULT SYSDATE) AS
 		nombre_original VARCHAR2(20);
-		fecha_original DATE;
 	BEGIN
-		SELECT Provedor.nombre INTO nombre_original FROM Provedor
-		WHERE Provedor.id = v_id;
-		
-		SELECT Provedor.fecha_registro INTO fecha_original FROM Provedor
-		WHERE Provedor.id = v_id;
+        IF v_nombre = 'NULL' THEN
+            SELECT Provedor.nombre INTO nombre_original FROM Provedor
+            WHERE Provedor.id = v_id;
+        ELSE
+            nombre_original := v_nombre;
+        END IF;
 		
 		UPDATE Provedor
 		SET
-			nombre = NULLIF(v_nombre, nombre_original),
-			fecha_registro = NULLIF(v_fecha, fecha_original)
+			nombre = nombre_original,
+			fecha_registro = v_fecha
 		WHERE
 			Provedor.id = v_id;
 	END;
 	
-	PROCEDURE modificar_producto(v_id NUMBER, v_nombre VARCHAR2, v_precio NUMBER, v_descp VARCHAR2) AS
+	PROCEDURE modificar_producto(v_id NUMBER, v_nombre VARCHAR2 DEFAULT 'NULL', v_precio NUMBER DEFAULT 0, v_descp VARCHAR2 DEFAULT 'NULL') AS
 		nombre_original VARCHAR2(20);
 		descripcion_original VARCHAR2(100);
 		precio_original NUMBER(6,2);
 	BEGIN
-		SELECT Producto.nombre INTO nombre_original FROM Producto
-		WHERE Producto.id = v_id;
+        IF v_nombre = 'NULL' THEN
+            SELECT Producto.nombre INTO nombre_original FROM Producto
+            WHERE Producto.id = v_id;
+        ELSE
+            nombre_original := v_nombre;
+        END IF;
+        
+		IF v_descp = 'NULL' THEN
+            SELECT Producto.descripcion INTO descripcion_original FROM Producto
+            WHERE Producto.id = v_id;
+        ELSE
+            descripcion_original := v_descp;
+        END IF;
 		
-		SELECT Producto.descripcion INTO descripcion_original FROM Producto
-		WHERE Producto.id = v_id;
-		
-		SELECT Producto.precio INTO precio_original FROM Producto
-		WHERE Producto.id = v_id;
+		IF v_precio = 0 THEN
+            SELECT Producto.precio INTO precio_original FROM Producto
+            WHERE Producto.id = v_id;
+        ELSE
+            precio_original := v_precio;
+        END IF;
 		
 		UPDATE Producto
 		SET
-			nombre = NULLIF(v_nombre, nombre_original),
-			descripcion = NULLIF(v_descp, descripcion_original),
-			precio = NULLIF(v_precio, precio_original)
+			nombre =  nombre_original,
+			descripcion = descripcion_original,
+			precio = precio_original
 		WHERE
 			Producto.id = v_id;
 	END;
 	
-	PROCEDURE modificar_cliente(v_id NUMBER, v_nombre VARCHAR2, v_apellido1 VARCHAR2,  v_apellido2 VARCHAR2) AS
+	PROCEDURE modificar_cliente(v_id NUMBER, 
+        v_nombre VARCHAR2 DEFAULT 'NULL', 
+        v_apellido1 VARCHAR2 DEFAULT 'NULL',
+        v_apellido2 VARCHAR2 DEFAULT 'NULL') 
+    AS
 		nombre_original VARCHAR2(20);
 		apellido1_original VARCHAR2(20);
 		apellido2_original VARCHAR2(20);
 	BEGIN
-		SELECT nombre INTO nombre_original FROM Cliente
-		WHERE id = v_id AND	v_id IS NOT NULL;
 		
-		SELECT apellido1 INTO apellido1_original FROM Cliente
-		WHERE id = v_id AND	v_id IS NOT NULL;
-		
-		SELECT apellido2 INTO apellido2_original FROM Cliente
-		WHERE id = v_id AND	v_id IS NOT NULL;
+        IF v_nombre = 'NULL' THEN
+            SELECT nombre INTO nombre_original FROM Cliente
+            WHERE id = v_id;
+        ELSE
+            nombre_original := v_nombre;
+        END IF;
+        
+        IF v_apellido1 = 'NULL' THEN
+            SELECT apellido1 INTO apellido1_original FROM Cliente
+            WHERE id = v_id;
+        ELSE
+            apellido1_original := v_apellido1;
+        END IF;
+        
+        IF v_apellido2 = 'NULL' THEN
+            SELECT apellido2 INTO apellido2_original FROM Cliente
+            WHERE id = v_id;
+        ELSE
+            apellido2_original := v_apellido2;
+        END IF;
 		
 		UPDATE Cliente
 		SET
-			nombre = NULLIF(v_nombre, nombre_original),
-			apellido1 = NULLIF(v_apellido1, apellido1_original),
-			apellido2 = NULLIF(v_apellido2, apellido2_original)
+			nombre = nombre_original,
+			apellido1 = apellido1_original,
+			apellido2 = apellido2_original
 		WHERE
-			id = v_id AND
-			v_id IS NOT NULL;
+			id = v_id;
 	END;
 	
-	PROCEDURE modificar_empleado(v_id NUMBER, v_nombre VARCHAR2, v_apellido1 VARCHAR2,
-		v_apellido2 VARCHAR2, v_salario NUMBER, v_correo VARCHAR2, v_contacto VARCHAR2) 
+	PROCEDURE modificar_empleado(v_id NUMBER, 
+        v_nombre VARCHAR2 DEFAULT 'NULL', 
+        v_apellido1 VARCHAR2 DEFAULT 'NULL', 
+        v_apellido2 VARCHAR2 DEFAULT 'NULL', 
+        v_salario NUMBER DEFAULT 0, 
+        v_correo VARCHAR2 DEFAULT 'NULL', 
+        v_contacto VARCHAR2 DEFAULT 'NULL') 
     AS
 		nombre_original VARCHAR2(20);
 		apellido1_original VARCHAR2(20);
@@ -170,35 +203,58 @@ AS
 		correo_original VARCHAR2(20);
 		contacto_original VARCHAR2(9);
 	BEGIN
-		SELECT nombre INTO nombre_original FROM Empleado
-		WHERE id = v_id AND	v_id IS NOT NULL;
+        IF v_nombre = 'NULL' THEN
+            SELECT nombre INTO nombre_original FROM Empleado
+            WHERE id = v_id;
+        ELSE
+            nombre_original := v_nombre;
+        END IF;
+        
+        IF v_apellido1 = 'NULL' THEN
+            SELECT apellido1 INTO apellido1_original FROM Empleado
+            WHERE id = v_id;
+        ELSE
+            apellido1_original := v_apellido1;
+        END IF;
+        
+        IF v_apellido2 = 'NULL' THEN
+            SELECT apellido2 INTO apellido2_original FROM Empleado
+            WHERE id = v_id;
+        ELSE
+            apellido2_original := v_apellido2;
+        END IF;
 		
-		SELECT apellido1 INTO apellido1_original FROM Empleado
-		WHERE id = v_id AND	v_id IS NOT NULL;
+		IF v_salario = 0 THEN
+            SELECT salario INTO salario_original FROM Empleado
+            WHERE id = v_id;
+        ELSE
+            salario_original := v_salario;
+        END IF;
 		
-		SELECT apellido2 INTO apellido2_original FROM Empleado
-		WHERE id = v_id AND	v_id IS NOT NULL;
+		IF v_correo = 'NULL' THEN
+            SELECT correo INTO correo_original FROM Empleado
+            WHERE id = v_id;
+        ELSE
+            correo_original := v_correo;
+        END IF;
 		
-		SELECT salario INTO salario_original FROM Empleado
-		WHERE id = v_id AND	v_id IS NOT NULL;
-		
-		SELECT correo INTO correo_original FROM Empleado
-		WHERE id = v_id AND	v_id IS NOT NULL;
-		
-		SELECT contacto INTO contacto_original FROM Empleado
-		WHERE id = v_id AND	v_id IS NOT NULL;
+		IF v_contacto = 'NULL' THEN
+            SELECT contacto INTO contacto_original FROM Empleado
+            WHERE id = v_id;
+        ELSE
+            contacto_original := v_contacto;
+        END IF;
 		
 		UPDATE Empleado
 		SET
-			nombre = NULLIF(v_nombre, nombre_original),
-			apellido1 = NULLIF(v_apellido1, apellido1_original),
-			apellido2 = NULLIF(v_apellido2, apellido2_original),
-			salario = NULLIF(v_salario, salario_original),
-			correo = NULLIF(v_correo, correo_original),
-			contacto = NULLIF(v_contacto, contacto_original)
+			nombre = nombre_original,
+			apellido1 = apellido1_original,
+			apellido2 = apellido2_original,
+			salario = salario_original,
+			correo = correo_original,
+			contacto = contacto_original
 		WHERE
-			id = v_id AND
-			v_id IS NOT NULL;
+			id = v_id;
 	END;
 	
 END package_CRUD;
