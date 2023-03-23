592e72581b1c1850d24eb36acfc0e77340c18239
DavidEspinozaNemo <david1998es16@gmail.com>
2023-03-22 20:49:27 -0600

    update ventas_mes

--- SQL Files/p_assistant.sql
+++ SQL Files/p_assistant.sql
@@ -5,6 +5,7 @@ CREATE OR REPLACE PACKAGE package_assistant AS
     PROCEDURE get_customers_more_shopping (t_result OUT SYS_REFCURSOR);
     PROCEDURE get_employees_more_sales (t_result OUT SYS_REFCURSOR);
     PROCEDURE get_total_sales_per_month (t_result OUT SYS_REFCURSOR);
+    FUNCTION ventas_mes(v_fecha IN VARCHAR2) RETURN NUMBER;
 END package_assistant;
 /
 
@@ -77,6 +78,28 @@ AS
         group by EXTRACT(MONTH FROM  fecha)
         order by ventas desc;
     END;
+    
+    -- Funcion que resive una fecha en VARCHAR2 y devuelve el total de ventas por el mes indicado
+    FUNCTION ventas_mes(v_fecha IN VARCHAR2)
+    RETURN NUMBER
+    IS
+      v_mes DATE;
+      v_total NUMBER;
+    BEGIN
+      -- Convertir el mes en formato de fecha
+      v_mes := TO_DATE( v_fecha, 'DD-MON-YYYY');
+      
+      -- Calcular el total de ventas del mes
+      SELECT SUM(v.monto_total)
+      INTO v_total
+      FROM Venta v
+      INNER JOIN producto_venta pv ON pv.id_venta = v.id
+      WHERE TO_CHAR(v.fecha, 'MM-YYYY') = TO_CHAR(v_mes, 'MM-YYYY');
+      
+      -- Devolver el total de ventas
+      RETURN v_total;
+      
+    END ventas_mes;
 	
 END package_assistant;
 /
@@ -86,3 +109,6 @@ END package_assistant;
 -- variable rc refcursor;
 -- EXEC package_assistant.get_more_sell( :rc );
 -- print rc;
+
+-- Para llamar a la función
+-- SELECT ventas_mes('7 marzo 2002') AS ventas_marzo FROM DUAL;
