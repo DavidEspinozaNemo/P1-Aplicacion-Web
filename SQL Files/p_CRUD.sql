7f8233a9b124de5796c82cbda7bd395c46da8253
DavidEspinozaNemo <david1998es16@gmail.com>
2023-03-22 20:52:33 -0600

    Update p_CRUD.sql

--- SQL Files/p_CRUD.sql
+++ SQL Files/p_CRUD.sql
@@ -10,6 +10,15 @@ CREATE OR REPLACE PACKAGE package_CRUD AS
     PROCEDURE modificar_producto(v_id NUMBER, v_nombre VARCHAR2 DEFAULT 'NULL', v_precio NUMBER DEFAULT 0, v_descp VARCHAR2 DEFAULT 'NULL');
     PROCEDURE modificar_cliente(v_id NUMBER, v_nombre VARCHAR2 DEFAULT 'NULL', v_apellido1 VARCHAR2 DEFAULT 'NULL',  v_apellido2 VARCHAR2 DEFAULT 'NULL');
     PROCEDURE modificar_empleado(v_id NUMBER, v_nombre VARCHAR2 DEFAULT 'NULL', v_apellido1 VARCHAR2 DEFAULT 'NULL', v_apellido2 VARCHAR2 DEFAULT 'NULL', v_salario NUMBER DEFAULT 0, v_correo VARCHAR2 DEFAULT 'NULL', v_contacto VARCHAR2 DEFAULT 'NULL');
+    PROCEDURE eliminar_cliente( v_idcliente number );
+    PROCEDURE eliminar_venta( v_idventa number );
+    PROCEDURE eliminar_provedor( v_idprovedor number );
+    PROCEDURE eliminar_producto_venta( v_id_prod_venta number );
+    PROCEDURE eliminar_producto( v_idproducto number );
+    PROCEDURE eliminar_empleado( v_idempleado number );
+    PROCEDURE eliminar_historial_compra( v_id_hist_compra number );
+    PROCEDURE eliminar_bitacora_salario( v_id_bit_salar number );
+    PROCEDURE eliminar_bitacora_producto( v_id_bit_prod number );
 END package_CRUD;
 /
 
@@ -19,6 +28,7 @@ AS
 	PROCEDURE agregar_venta (v_fecha date, v_monto NUMBER, v_EmpleadoID NUMBER, v_clienteID number) as
 	BEGIN
 		INSERT INTO Venta(fecha, monto_total, empleado, cliente) VALUES (v_fecha, v_monto, v_EmpleadoID, v_clienteID);
+        COMMIT;
 		EXCEPTION WHEN OTHERS THEN
 		IF SQLCODE=-1 THEN
 			DBMS_OUTPUT.put_line ('Ha ocurrido un error al registrar la venta');
@@ -30,7 +40,8 @@ AS
 	PROCEDURE agregar_empleado (v_nombre varchar2, v_apellido1 varchar2, v_apellido2 varchar2, v_salario number, v_fecha date, v_correo varchar2, v_contacto varchar2) as
 	BEGIN
 		INSERT INTO Empleado(nombre, apellido1, apellido2, salario, fecha, correo, contacto) VALUES (v_nombre, v_apellido1, v_apellido2, v_salario, v_fecha, v_correo, v_contacto);
-		EXCEPTION WHEN OTHERS THEN
+		COMMIT;
+        EXCEPTION WHEN OTHERS THEN
 		IF SQLCODE=-1 THEN
 			DBMS_OUTPUT.put_line ('Ha ocurrido un error al registrar al empleado');
 		ELSE
@@ -41,6 +52,7 @@ AS
 	PROCEDURE agregar_cliente (v_nombre varchar2, v_apellido1 varchar2, v_apellido2 varchar2, v_fecha date) as
 	BEGIN
 		INSERT INTO Cliente(nombre, apellido1, apellido2, fecha_registro) VALUES (v_nombre, v_apellido1, v_apellido2, v_fecha);
+        COMMIT;
 		EXCEPTION WHEN OTHERS THEN
 		IF SQLCODE=-1 THEN
 			DBMS_OUTPUT.put_line ('Ha ocurrido un error al registrar al cliente');
@@ -52,6 +64,7 @@ AS
 	PROCEDURE agregar_provedor (v_nombre varchar2,v_fecha date) as
 	BEGIN
 		INSERT INTO Provedor(nombre, fecha_registro) VALUES (v_nombre, v_fecha);
+        COMMIT;
 		EXCEPTION WHEN OTHERS THEN
 		IF SQLCODE=-1 THEN
 			DBMS_OUTPUT.put_line ('Ha ocurrido un error al registrar al provedor');
@@ -63,6 +76,7 @@ AS
 	PROCEDURE agregar_producto (v_nombre varchar2,v_descripcion varchar2, v_precio number) as
 	BEGIN
 		INSERT INTO Producto(nombre, descripcion, precio) VALUES (v_nombre, v_descripcion, v_precio);
+        COMMIT;
 		EXCEPTION WHEN OTHERS THEN
 		IF SQLCODE=-1 THEN
 			DBMS_OUTPUT.put_line ('Ha ocurrido un error al registrar el producto');
@@ -74,6 +88,7 @@ AS
 	PROCEDURE agregar_hcompra (v_fecha date,v_costo number, v_cantidad number, v_provedorID number) as
 	BEGIN
 		INSERT INTO Historial_Compra(fecha, costo, cantidad, provedor) VALUES (v_fecha, v_costo, v_cantidad, v_provedorID);
+        COMMIT;
 		EXCEPTION WHEN OTHERS THEN
 		IF SQLCODE=-1 THEN
 			DBMS_OUTPUT.put_line ('Ha ocurrido un error al registrar la bitacora');
@@ -85,6 +100,7 @@ AS
 	PROCEDURE agregar_productoV (v_productoID number,v_ventaID number, v_precio number, v_cantidad number) as
 	BEGIN
 		INSERT INTO producto_venta(id_producto, id_venta, precio, cantidad ) VALUES (v_productoID, v_ventaID, v_precio, v_cantidad);
+        COMMIT;
 		EXCEPTION WHEN OTHERS THEN
 		IF SQLCODE=-1 THEN
 			DBMS_OUTPUT.put_line ('Ha ocurrido un error al registrar el producto a la venta');
@@ -111,6 +127,7 @@ AS
 			fecha_registro = v_fecha
 		WHERE
 			Provedor.id = v_id;
+        COMMIT;
 	END;
 	
 	PROCEDURE modificar_producto(v_id NUMBER, v_nombre VARCHAR2 DEFAULT 'NULL', v_precio NUMBER DEFAULT 0, v_descp VARCHAR2 DEFAULT 'NULL') AS
@@ -146,6 +163,7 @@ AS
 			precio = precio_original
 		WHERE
 			Producto.id = v_id;
+        COMMIT;
 	END;
 	
 	PROCEDURE modificar_cliente(v_id NUMBER, 
@@ -186,6 +204,7 @@ AS
 			apellido2 = apellido2_original
 		WHERE
 			id = v_id;
+        COMMIT;
 	END;
 	
 	PROCEDURE modificar_empleado(v_id NUMBER, 
@@ -255,7 +274,118 @@ AS
 			contacto = contacto_original
 		WHERE
 			id = v_id;
+        COMMIT;
 	END;
+    
+    -- proceimientos para borrar los registros
+    -- eliminar cliente
+    PROCEDURE eliminar_cliente( v_idcliente number )
+    IS
+    BEGIN
+        DELETE FROM Cliente
+            WHERE id = v_idcliente;
+            COMMIT;
+            DBMS_OUTPUT.PUT_LINE('Cliente eliminado exitosamente');
+        EXCEPTION WHEN OTHERS THEN
+            ROLLBACK;        
+            DBMS_OUTPUT.PUT_LINE('Error al eliminar el cliente');
+    END;
+    -- eliminar bitacora producto
+    PROCEDURE eliminar_bitacora_producto( v_id_bit_prod number )
+    IS
+    BEGIN
+        DELETE FROM bitacora_producto
+            WHERE id = v_id_bit_prod;
+            COMMIT;
+            DBMS_OUTPUT.PUT_LINE('Registro de la bitacora de producto ha sido eliminado exitosamente');
+        EXCEPTION WHEN OTHERS THEN
+            ROLLBACK;        
+            DBMS_OUTPUT.PUT_LINE('Error al eliminar el registro de la bitacora de producto');
+    END;
+    -- eliminar bitacora salarios
+    PROCEDURE eliminar_bitacora_salario( v_id_bit_salar number )
+    IS
+    BEGIN
+        DELETE FROM bitacora_salarios
+            WHERE id = v_id_bit_salar;
+            COMMIT;
+            DBMS_OUTPUT.PUT_LINE('Registro de la bitacora de salarios eliminado exitosamente');
+        EXCEPTION WHEN OTHERS THEN
+            ROLLBACK;        
+            DBMS_OUTPUT.PUT_LINE('Error al eliminar el registro de la bitacora de salarios');
+    END;
+    -- eliminar empleado
+    PROCEDURE eliminar_empleado( v_idempleado number )
+    IS
+    BEGIN
+        DELETE FROM empleado
+            WHERE id = v_idempleado;
+            COMMIT;
+            DBMS_OUTPUT.PUT_LINE('Empleado eliminado exitosamente');
+        EXCEPTION WHEN OTHERS THEN
+            ROLLBACK;        
+            DBMS_OUTPUT.PUT_LINE('Error al eliminar el empleado');
+    END;
+    -- eliminar historial compra
+    PROCEDURE eliminar_historial_compra( v_id_hist_compra number )
+    IS
+    BEGIN
+        DELETE FROM historial_compra
+            WHERE id = v_id_hist_compra;
+            COMMIT;
+            DBMS_OUTPUT.PUT_LINE('Registro del historial de compra eliminado exitosamente');
+        EXCEPTION WHEN OTHERS THEN
+            ROLLBACK;        
+            DBMS_OUTPUT.PUT_LINE('Error al eliminar el registro del historial de compra');
+    END;
+    -- eliminar producto
+    PROCEDURE eliminar_producto( v_idproducto number )
+    IS
+    BEGIN
+        DELETE FROM producto
+            WHERE id = v_idproducto;
+            COMMIT;
+            DBMS_OUTPUT.PUT_LINE('Producto eliminado exitosamente');
+        EXCEPTION WHEN OTHERS THEN
+            ROLLBACK;        
+            DBMS_OUTPUT.PUT_LINE('Error al eliminar el producto');
+    END;
+    -- eliminar producto venta
+    PROCEDURE eliminar_producto_venta( v_id_prod_venta number )
+    IS
+    BEGIN
+        DELETE FROM producto_venta
+            WHERE id_registro = v_id_prod_venta;
+            COMMIT;
+            DBMS_OUTPUT.PUT_LINE('Registro de producto venta eliminado exitosamente');
+        EXCEPTION WHEN OTHERS THEN
+            ROLLBACK;        
+            DBMS_OUTPUT.PUT_LINE('Error al eliminar el registro de producto venta');
+    END;
+    -- eliminar provedor
+    PROCEDURE eliminar_provedor( v_idprovedor number )
+    IS
+    BEGIN
+        DELETE FROM provedor
+            WHERE id = v_idprovedor;
+            COMMIT;
+            DBMS_OUTPUT.PUT_LINE('Provedor eliminado exitosamente');
+        EXCEPTION WHEN OTHERS THEN
+            ROLLBACK;        
+            DBMS_OUTPUT.PUT_LINE('Error al eliminar el provedor');
+    END;
+    -- eliminar venta
+    PROCEDURE eliminar_venta( v_idventa number )
+    IS
+    BEGIN
+        DELETE FROM venta
+            WHERE id = v_idventa;
+            COMMIT;
+            DBMS_OUTPUT.PUT_LINE('Venta eliminado exitosamente');
+        EXCEPTION WHEN OTHERS THEN
+            ROLLBACK;        
+            DBMS_OUTPUT.PUT_LINE('Error al eliminar el venta');
+    END;
 	
 END package_CRUD;
 /
