7d79ee08a4eae4391415b27874f2730ababd1fc6
DavidEspinozaNemo <david1998es16@gmail.com>
2023-03-24 13:42:50 -0600

    procedimientos read

--- SQL Files/p_assistant.sql
+++ SQL Files/p_assistant.sql
@@ -6,6 +6,15 @@ CREATE OR REPLACE PACKAGE package_assistant AS
     PROCEDURE get_employees_more_sales (t_result OUT SYS_REFCURSOR);
     PROCEDURE get_total_sales_per_month (t_result OUT SYS_REFCURSOR);
     FUNCTION ventas_mes(v_fecha IN VARCHAR2) RETURN NUMBER;
+    PROCEDURE obtener_empleados;
+    PROCEDURE obtener_BitacoraP;
+    PROCEDURE obtener_BitacoraS;
+    PROCEDURE obtener_clientes;
+    PROCEDURE obtener_HistorialCompra;
+    PROCEDURE obtener_Productos;
+    PROCEDURE obtener_ProductosVentas;
+    PROCEDURE obtener_Provedor;
+    PROCEDURE obtener_Ventas;
 END package_assistant;
 /
 
@@ -100,14 +109,87 @@ AS
       RETURN v_total;
       
     END ventas_mes;
+    
+    PROCEDURE obtener_empleados AS
+    BEGIN  
+        FOR empleados IN (SELECT * FROM EMPLEADO)
+        LOOP    -- Realiza la acción que desees con cada registro, por ejemplo imprimir sus valores
+            DBMS_OUTPUT.PUT_LINE(empleados.ID || '-' || empleados.nombre || '-' ||empleados.apellido1 || '-' || empleados.apellido2 || '-' || empleados.salario|| '-' || empleados.fecha || '-' || empleados.correo || '-' || empleados.contacto); 
+        END LOOP;
+    END;
+    
+    PROCEDURE obtener_BitacoraP IS
+    BEGIN
+      FOR BP IN (SELECT * FROM bitacora_producto)  LOOP
+        -- Realiza la acción que desees con cada registro, por ejemplo imprimir sus valores    
+        DBMS_OUTPUT.PUT_LINE(BP.ID || '-' || BP.ID_PRODUCTO  || '-' || BP.PRECIO_VIEJO || '-' ||  BP.NOMBRE_PRODUCTO  || '-' ||  BP.PRECIO_NUEVO  || '-' ||BP.FECHA_CAMBIO);
+      END LOOP;
+    END;
+    
+    PROCEDURE obtener_BitacoraS IS
+    BEGIN
+      FOR BP IN (SELECT * FROM bitacora_salarios)  LOOP
+        -- Realiza la acción que desees con cada registro, por ejemplo imprimir sus valores    
+        DBMS_OUTPUT.PUT_LINE(BP.ID || '-' || BP.fecha_cambio  || '-' || BP.usuario|| '-' ||  BP.empleado || '-' || BP.salario_anterior  || '-' ||  BP.Salario_nuevo);
+      END LOOP;
+    END;
+    
+    PROCEDURE obtener_clientes IS
+    BEGIN
+      FOR c IN (SELECT * FROM Cliente)  LOOP
+        -- Realiza la acción que desees con cada registro, por ejemplo imprimir sus valores    
+        DBMS_OUTPUT.PUT_LINE(c.ID || '-' ||  c.nombre || '-' ||  c.apellido1 || '-' ||  c.apellido2 || '-' ||  c.fecha_registro);
+      END LOOP;
+    END;
+    
+    PROCEDURE obtener_HistorialCompra IS
+    BEGIN
+      FOR hc IN (SELECT * FROM historial_compra)  LOOP
+        -- Realiza la acción que desees con cada registro, por ejemplo imprimir sus valores    
+        DBMS_OUTPUT.PUT_LINE(hc.ID  || '-' || hc.fecha  || '-' || hc.costo || '-' || hc.cantidad|| '-' ||  hc.provedor || '-' ||  hc.producto);
+      END LOOP;
+    END;
+    
+    PROCEDURE obtener_Productos IS
+    BEGIN
+      FOR P IN (SELECT * FROM Producto)  LOOP
+        -- Realiza la acción que desees con cada registro, por ejemplo imprimir sus valores    
+        DBMS_OUTPUT.PUT_LINE(P.ID  || '-' || P.nombre  || '-' ||  P.descripcion  || '-' ||  P.precio);
+      END LOOP;
+    END;
 	
+    PROCEDURE obtener_ProductosVentas IS
+    BEGIN
+        FOR PV IN (SELECT * FROM producto_venta)
+        LOOP    -- Realiza la acción que desees con cada registro, por ejemplo imprimir sus valores
+            DBMS_OUTPUT.PUT_LINE(PV.ID_REGISTRO  || '-' ||  PV.ID_PRODUCTO  || '-' ||PV.ID_VENTA|| '-' ||  PV.PRECIO || '-' || PV.CANTIDAD);  
+        END LOOP;
+    END;
+    
+    PROCEDURE obtener_Provedor IS
+    BEGIN
+        FOR PV IN (SELECT * FROM provedor)  
+        LOOP        -- Realiza la acción que desees con cada registro, por ejemplo imprimir sus valores    
+            DBMS_OUTPUT.PUT_LINE(PV.ID || '-' ||  PV.NOMBRE || '-' || PV.FECHA_REGISTRO);
+        END LOOP;
+    END;
+    
+    PROCEDURE obtener_Ventas IS
+    BEGIN
+        FOR V IN (SELECT * FROM VENTA)  
+        LOOP
+            -- Realiza la acción que desees con cada registro, por ejemplo imprimir sus valores    
+            DBMS_OUTPUT.PUT_LINE(V.ID || '-' || V.FECHA || '-' || V.MONTO_TOTAL || '-' ||V.EMPLEADO || '-' || V.CLIENTE);
+        END LOOP;
+    END;
+    
 END package_assistant;
 /
 
 -- como ejecutar estos procedimientos
 -- set serveroutput on;
 -- variable rc refcursor;
--- EXEC package_assistant.get_more_sell( :rc );
+-- EXEC package_assistant.get_best_seller( :rc );
 -- print rc;
 
 -- Para llamar a la función
